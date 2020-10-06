353d5612774bf3393d598767b76c15a334d4870c
Ayushish93 <ayushi08sharma@gmail.com>
2020-10-06 13:43:52 +0000

    implemented counter functionality

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,16 @@
+// Add keyup listener on textarea and updating counter value with the length of textarea
+$('document').ready(function(){
+  let count = 0;
+  $("#tweet-text").keyup(function(data) {
+    var comment = $("#tweet-text").val();
+    count = 140 - (comment.length );
+    $(".counter").val(count);
+    if(count <= 0) {
+      $(".counter").addClass("red");
+    }
+    else {
+      $(".counter").addClass("black");
+    }
+    
+  });
+});
