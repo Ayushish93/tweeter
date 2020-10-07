fa566d9c29d5d6ff757173a775a0293a2097dbb1
Ayushish93 <ayushi08sharma@gmail.com>
2020-10-07 01:04:32 +0000

    Added html template in client.js to add dynamic data on page

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,5 +3,78 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+$('document').ready(function(){
 
 
+// Fake data taken from initial-tweets.json
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+]
+
+let $tweetdata = $('#tweet-container');
+const renderTweets = function(data) {
+  // loops through tweets
+  data.forEach((user) => {
+    console.log("user", user);
+  // calls createTweetElement for each tweet
+    return $tweetdata.append(createTweetElement(user));
+  });
+  
+} 
+
+const createTweetElement = function(data) {
+ 
+  return `
+  <article class="tweeter-new-post">
+    <header>
+      <div class="tweet-contact-info">
+        <span> <img src = ${data['user'].avatars}> </span>
+        <span class="tweet-contact-name"> ${data['user'].name} </span>
+      </div>
+      <div class="tweet-id"> ${data['user'].handle} </div>
+    </header>
+    <article > 
+      <p id="tweetdata">${data['content'].text} </p>
+    </article>
+    <footer>
+      <div>
+        <span> 10 Day ago </span>
+      </div>
+      <div class="footer-icons">
+        <span> <i class="fa fa-flag" aria-hidden="true"></i> </span>
+        <span> <i class="fa fa-heart" aria-hidden="true"></i> </span>
+        <span> <i class="fa fa-retweet" aria-hidden="true"></i> </span>
+      </div>
+    </footer>
+  </article>`
+}
+
+
+  
+renderTweets(data);
+
+
+
+
+});
